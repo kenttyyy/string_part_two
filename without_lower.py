6df5032035348cb776c80257e140ba0bb703f2f5
kenttyyy <kentajrins123@gmail.com>
2025-03-28 17:49:08 +0800

    replaced character

---
+++ without_lower.py
@@ -0,0 +1,7 @@
+# Sample text
+text = "HeLLo WoRLD"
+
+# Makes the text lower without using lower
+result = text.replace("H", "h").replace("L", "l").replace("W", "w").replace("R", "r").replace("D", "d")
+
+print(result)
